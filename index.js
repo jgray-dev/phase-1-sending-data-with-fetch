5614e6060ae94587d37051bdc74b6be8506482f4
Jackson Gray <nohaxjustdoge@gmail.com>
2024-02-26 20:08:06 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  return fetch("http://67.164.191.36:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const body = (document.querySelector("body").textContent = data.id);
+    })
+    .catch((error) => {
+      const body = (document.querySelector("body").textContent = error.message);
+    });
+}
--- test/helpers.js
+++ test/helpers.js
@@ -1,22 +1,26 @@
-const chai = require( 'chai' )
-global.expect = chai.expect
-const fs = require( 'fs' )
-const jsdom = require( 'mocha-jsdom' )
-const path = require( 'path' )
-const babel = require( 'babel-core' );
+const chai = require("chai");
+global.expect = chai.expect;
+const fs = require("fs");
+const jsdom = require("mocha-jsdom");
+const path = require("path");
+const babel = require("babel-core");
 
-const html = fs.readFileSync( path.resolve( __dirname, '..', 'index.html' ), 'utf-8' )
-
-const babelResult = babel.transformFileSync(
-  path.resolve( __dirname, '..', 'index.js' ), {
-    presets: [ 'env' ]
-  }
+const html = fs.readFileSync(
+  path.resolve(__dirname, "..", "index.html"),
+  "utf-8",
 );
 
-const src = babelResult.code
+const babelResult = babel.transformFileSync(
+  path.resolve(__dirname, "..", "index.js"),
+  {
+    presets: ["env"],
+  },
+);
 
-jsdom( {
+const src = babelResult.code;
+
+jsdom({
   html,
   src,
-  url: "http://localhost"
-} );
+  url: "http://67.164.191.36",
+});
